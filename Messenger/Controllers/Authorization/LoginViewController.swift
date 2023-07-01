4a520f8cdd05a26c23090b2c7b652103e964641e
a-shdv <shadaev2001@icloud.com>
2023-07-01 21:31:15 +0400

    Update LoginViewController

---
+++ Messenger/Controllers/Authorization/LoginViewController.swift
@@ -0,0 +1,114 @@
+//
+//  LoginViewController.swift
+//  Messenger
+//
+//  Created by Anton Shadaev on 01.07.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "Logo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let emailField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "E-Mail"
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        return field
+    }()
+    
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .done
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Пароль"
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+        let loginButton = UIButton()
+        loginButton.setTitle("Войти", for: .normal)
+        loginButton.backgroundColor = .link
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.layer.cornerRadius = 12
+        loginButton.layer.masksToBounds = true
+        loginButton.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        return loginButton
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Войти"
+        view.backgroundColor = .white
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Войти",
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(didTapRegister)
+        )
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(passwordField)
+        scrollView.addSubview(loginButton)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.frame = view.bounds
+        let size = scrollView.width / 3
+        imageView.frame = CGRect(x:  (scrollView.width - size) / 2,
+                                 y:  20,
+                                 width: size,
+                                 height: size)
+        
+        emailField.frame = CGRect(x: 30,
+                                  y:  imageView.bottom + 10,
+                                  width: scrollView.width - 60,
+                                  height: 52)
+        
+        passwordField.frame = CGRect(x: 30,
+                                     y:  emailField.bottom + 10,
+                                     width: scrollView.width - 60,
+                                     height: 52)
+        
+        loginButton.frame = CGRect(x: 30,
+                                   y:  passwordField.bottom + 10,
+                                   width: scrollView.width - 60,
+                                   height: 52)
+    }
+    
+    @objc private func didTapRegister() {
+        let registerVC = RegisterViewController()
+        registerVC.title = "Зарегистрироваться"
+        navigationController?.pushViewController(registerVC, animated: true)
+    }
+    
+}
---
+++ Messenger/Controllers/Authorization/RegisterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterViewController.swift
+//  Messenger
+//
+//  Created by Anton Shadaev on 01.07.2023.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Зарегистрироваться"
+        
+        view.backgroundColor =  .link
+        
+    }
+    
+}

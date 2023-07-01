3e61980e1a237e6c35218ee1fd75c987a4ee936d
a-shdv <shadaev2001@icloud.com>
2023-07-01 18:33:42 +0400

    Initial commit

---
+++ messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  messenger
+//
+//  Created by Anton Shadaev on 01.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
